c0437ca2fbfe97e4a82171245d95f32db1f142cc
xxria17 <xxrcy32@gmail.com>
2023-09-11 16:04:08 +0900

    [Update] 검색 화면 ui 구현

---
+++ app/src/main/java/com/dhxxn17/escape96app/ui/pages/home/ThemeAdapter.kt
@@ -0,0 +1,52 @@
+package com.dhxxn17.escape96app.ui.pages.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.dhxxn17.escape96app.R
+import com.dhxxn17.escape96app.data.Theme
+
+class ThemeAdapter : RecyclerView.Adapter<ThemeAdapter.ThemeViewHolder>() {
+
+    private val list = mutableListOf<Theme>()
+    var onClick: (Theme) -> Unit = {}
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ThemeAdapter.ThemeViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_theme, parent, false)
+        return ThemeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ThemeAdapter.ThemeViewHolder, position: Int) {
+        val result = list[position]
+        holder.bind(result)
+        holder.itemView.setOnClickListener { onClick(result) }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    inner class ThemeViewHolder(view: View?): RecyclerView.ViewHolder(view!!) {
+        private val image: ImageView = view!!.findViewById(R.id.item_theme_image)
+        private val title: TextView = view!!.findViewById(R.id.item_theme_title)
+        private val location: TextView = view!!.findViewById(R.id.item_theme_location)
+
+        fun bind(theme: Theme) {
+            Glide.with(itemView)
+                .load(theme.thumbnail)
+                .centerCrop()
+                .into(image)
+
+            title.text = theme.title
+            location.text = theme.location
+        }
+    }
+}
